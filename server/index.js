0cb11f945876d9427afe62c859c5a8b2012abf5a
Pankaj Kumar <122406409+pankajsmallarc@users.noreply.github.com>
2023-11-25 19:39:35 +0530

    create middleware

--- server/index.js
+++ server/index.js
@@ -18,3 +18,13 @@ app.listen(3000, () => {
 
 app.use('/api/v1', userRouter);
 app.use('/api/v1/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
